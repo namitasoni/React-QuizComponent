d041d5392b40f725941f286d9cfc214dbc6a0915
Namita Soni <Namita.Soni@contractor.p66.com>
2021-09-17 17:15:30 -0500

    CHECK

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import QuizQuestion from "./QuizQuestion.js"
+
 let quizData = require('./quiz_data.json')
 class Quiz extends Component {
   constructor(props){
@@ -9,7 +10,7 @@ class Quiz extends Component {
   }
   render() {
     return (
-      <QuizQuestion />
+      <QuizQuestion  />
     )
   }
 }
--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,10 +5,12 @@ render(){
   <main>
         <section>
           <p>// instruction text goes here</p>
+          <li>this.props.instruction_text
         </section>
         <section className="buttons">
           <ul>
-            //quiz question button logic goes here
+          //quiz question button logic goes here
+          <li>this.props.quiz_question.answer_options[0]
           </ul>
         </section>
       </main>
