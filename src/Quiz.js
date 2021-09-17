619d6277d48390ab7fdf19f3fc79fa142bd57127
Namita Soni <Namita.Soni@contractor.p66.com>
2021-09-17 15:26:24 -0500

    Check progress

---
+++ src/Quiz.js
@@ -0,0 +1,17 @@
+import React, {} from 'react'
+
+let quizData = require('./quiz_date.json')
+class Quiz extends Component {
+  render() {
+    return (
+      <div> QuizQuestion </div>
+    )
+    constructor(props){
+      supper(props)
+      this.state = {quiz_position: 1}
+
+    }
+  }
+}
+
+export default Quiz
